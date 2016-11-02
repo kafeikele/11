575208af99a5e7ff0361c180e9e31b6abbcb6733
xionglei15 <xl@putao.cn>
2016-11-02 17:19:42 +0800

    forth commit

--- 1.py
+++ 1.py
@@ -2,10 +2,10 @@
 
 import paramiko
 
-hostname = '192.168.1.240'
+hostname = '120.26.42.111'
 port     = 22
 username = 'root'
-password = 'putao123'
+password = 'Putao2015'
 Known_host = "C:\\Users\\Administrator\\.ssh\\known_hosts"
 
 
@@ -15,7 +15,7 @@ if __name__ == "__main__":
             s.load_system_host_keys(Known_host)
             s.connect(hostname, port, username, password)
             s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            stdin, stdout, stderr = s.exec_command('ps aux')
+            stdin, stdout, stderr = s.exec_command('ps aux |grep -E "java|nginx|uwsgi" |grep -v grep')
 
             a = stdout.read()
             lists = a.split('\n')
@@ -25,5 +25,4 @@ if __name__ == "__main__":
                     return i and i.strip()
                 ret = filter(not_empty,i)
                 if len(ret) < 10:continue
-                print [ret[0], ret[1], ret[10]]
-            s.close()
+                print [ret[0], ret[1], ret[8], ret[10]]
