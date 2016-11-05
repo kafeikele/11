f8a8ed7f3cb966c0e9da4cd5bbcfb3620c6787ff
kafeikele <1046641572@qq.com>
2016-11-05 18:31:06 +0800

    commit

---
+++ pt_moni/server/urls.py
@@ -0,0 +1,13 @@
+#coding: utf-8
+#from django.conf.urls import *
+from django.conf.urls import url
+from server.views.shengchan_111 import index
+
+"""
+    blog应用所有的url
+
+"""
+
+urlpatterns = [
+    url(r'^$',index, {"template_name": "test.html"}, name='test'),
+]
